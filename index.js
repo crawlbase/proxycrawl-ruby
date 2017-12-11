cd7424ae9549d2b356f5144725a729b4afa84aaa
ajimix <ajimix@gmail.com>
2017-12-11 23:03:28 +0100

    Adds proper path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Chrome, chromeStats } = require('src/chrome.js');
-const { ChromeLinkedIn } = require('src/chrome-linkedin.js');
-const { ChromeLambda } = require('src/chrome-lambda.js');
-const { Firefox, firefoxStats } = require('src/firefox.js');
-const { FirefoxLinkedIn } = require('src/firefox-linkedin.js');
+const { Chrome, chromeStats } = require('./src/chrome.js');
+const { ChromeLinkedIn } = require('./src/chrome-linkedin.js');
+const { ChromeLambda } = require('./src/chrome-lambda.js');
+const { Firefox, firefoxStats } = require('./src/firefox.js');
+const { FirefoxLinkedIn } = require('./src/firefox-linkedin.js');
 
 module.exports = {
   Chrome,
