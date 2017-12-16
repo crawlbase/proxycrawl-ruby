5e04cd59fad99212ceefbe8478ba58fc329f7cf9
ajimix <ajimix@gmail.com>
2017-12-16 11:03:11 +0100

    Adds test for firefox

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const { Chrome } = require('./index.js');
+const { Chrome, Firefox } = require('./index.js');
 const chromePath = process.platform === 'darwin' ? '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome' : '/usr/bin/google-chrome';
+const firefoxPath = process.platform === 'darwin' ? '/Applications/Firefox.app/Contents/MacOS/firefox' : '/usr/bin/firefox';
 let errors = 0;
 
 const statsMock = {
@@ -28,9 +29,27 @@ function launchChrome() {
   });
 }
 
+function launchFirefox() {
+  const chromeInstance = new Firefox({
+    stats: statsMock,
+    appPath: firefoxPath,
+    url: 'https://wedibit.com/pro-tester',
+    proxy: '167.88.10.42:8012'
+  });
+
+  return chromeInstance.start().then((result) => {
+    if (result.response.status !== 200) {
+      console.error('Invalid response: ', result.response.status);
+      console.log(result.body);
+      errors++;
+    }
+  });
+}
+
 const promises = [];
 for (let i = 0; i < 1; i++) {
   promises.push(launchChrome());
+  promises.push(launchFirefox());
 }
 
 Promise.all(promises).then(() => {
