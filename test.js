aa7d12fa07d9e606ae1f5aa1eec7f20d6b436ffb
ajimix <ajimix@gmail.com>
2017-12-20 22:57:43 +0100

    Adds chrome linkedin test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { Chrome, Firefox, FirefoxLinkedIn } = require('./index.js');
+const { Chrome, ChromeLinkedIn, Firefox, FirefoxLinkedIn } = require('./index.js');
 const chromePath = process.platform === 'darwin' ? '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome' : '/usr/bin/google-chrome';
 const firefoxPath = process.platform === 'darwin' ? '/Applications/Firefox.app/Contents/MacOS/firefox' : '/usr/bin/firefox';
 let errors = 0;
@@ -19,6 +19,25 @@ function launchChrome() {
     appPath: chromePath,
     url: 'https://wedibit.com/pro-tester',
     proxy: '167.88.10.42:8012'
+  });
+
+  return instance.start().then((result) => {
+    if (result.response.status !== 200) {
+      console.error('Invalid response: ', result.response.status);
+      console.log(result.body);
+      errors++;
+    }
+  });
+}
+
+function launchChromeLinkedIn() {
+  console.log('Launching Chrome LinkedIn');
+  const instance = new ChromeLinkedIn({
+    stats: statsMock,
+    appPath: chromePath,
+    url: 'https://www.linkedin.com/in/williamhgates/',
+    proxy: '167.88.10.42:8012',
+    linkedInJoinWall: 'v=2&AQHglkQ3zcdUfQAAAWBujZJkBAnbPC66mS8a1vwO808N2_UAfaFc5ttUSGpYizHx5jOcPtsc4C_zNeVA5HcTIlTPd-8doo0Q0Vr2EtJEFkEZzQX3A5c0arb6ZPgi9H8fo0Z9IevMcDE3MadCS5A4lREO4w_r31Yw_X4a2X18VozKvQE1yBNwMw'
   });
 
   return instance.start().then((result) => {
@@ -70,6 +89,7 @@ function launchFirefoxLinkedIn() {
 const promises = [];
 for (let i = 0; i < 1; i++) {
   promises.push(launchChrome());
+  promises.push(launchChromeLinkedIn());
   promises.push(launchFirefox());
   promises.push(launchFirefoxLinkedIn());
 }
