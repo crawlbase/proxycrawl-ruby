e9dd36e22e866f17e9d933f8c326518c791a0e62
ajimix <ajimix@gmail.com>
2018-06-21 10:15:26 +0200

    Adds ChromeAliexpress test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { Chrome, ChromeLinkedIn, Firefox, FirefoxLinkedIn } = require('./index.js');
+const { Chrome, ChromeLinkedIn, ChromeAliexpress, Firefox, FirefoxLinkedIn } = require('./index.js');
 const chromePath = process.platform === 'darwin' ? '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome' : '/usr/bin/google-chrome';
 const firefoxPath = process.platform === 'darwin' ? '/Applications/Firefox.app/Contents/MacOS/firefox' : '/usr/bin/firefox';
 let errors = 0;
@@ -38,6 +38,23 @@ function launchChromeLinkedIn() {
     url: 'https://www.linkedin.com/in/williamhgates/',
     proxy: '167.88.10.42:8012',
     linkedInJoinWall: 'v=2&AQHglkQ3zcdUfQAAAWBujZJkBAnbPC66mS8a1vwO808N2_UAfaFc5ttUSGpYizHx5jOcPtsc4C_zNeVA5HcTIlTPd-8doo0Q0Vr2EtJEFkEZzQX3A5c0arb6ZPgi9H8fo0Z9IevMcDE3MadCS5A4lREO4w_r31Yw_X4a2X18VozKvQE1yBNwMw'
+  });
+
+  return instance.start().then((result) => {
+    if (result.response.status !== 200) {
+      console.error('Invalid response: ', result.response.status);
+      console.log(result.body);
+      errors++;
+    }
+  });
+}
+
+function launchChromeAliexpress() {
+  console.log('Launching Chrome Aliexpress');
+  const instance = new ChromeAliexpress({
+    stats: statsMock,
+    appPath: chromePath,
+    url: 'https://www.aliexpress.com/category/100005433/camping-hiking/8.html?g=n'
   });
 
   return instance.start().then((result) => {
@@ -92,6 +109,7 @@ for (let i = 0; i < 1; i++) {
   promises.push(launchChromeLinkedIn());
   promises.push(launchFirefox());
   promises.push(launchFirefoxLinkedIn());
+  promises.push(launchChromeAliexpress());
 }
 
 Promise.all(promises).then(() => {
