bdb6a91f45c070a9510feeb8bd49aa6dc24db908
ajimix <ajimix@gmail.com>
2020-11-03 14:54:14 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "es6": true
+  env: {
+    node: true,
+    es6: true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "sourceType": "module"
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: module
   },
-  "rules": {
-    "no-useless-escape": 0,
-    "no-console": 0,
-    "linebreak-style": [
-      "error",
-      "unix"
+  rules: {
+    'no-useless-escape': 0,
+    'no-console': 0,
+    'linebreak-style': [
+      'error',
+      'unix',
     ],
-    "quotes": [
-      "error",
-      "single"
+    quotes: [
+      'error',
+      'single',
     ],
-    "semi": [
-      "error",
-      "always"
+    semi: [
+      'error',
+      'always',
     ]
   },
-  "globals": {
-    "Notification": true,
-    "HTMLIFrameElement": true
+  globals: {
+    Notification: true,
+    HTMLIFrameElement: true
   }
 };
