54f3c0801546498165ac787e7613b0cfd0cfa9f3
adria <git@adria.link>
2020-11-25 14:59:36 +0800

    build: adds prettier to eslint [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 8,
     sourceType: module,
