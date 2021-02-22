1302e397f4a6290f69baebdaaad2e3a020858f23
adria <git@adria.link>
2021-02-22 16:40:30 +0800

    feat: use postinstall script

---
+++ .postinstall.js
@@ -0,0 +1,19 @@
+// This is the package.json postinstall script.
+// It disables the execution on CI and also when is installed as dependency.
+const { exec } = require('child_process');
+
+if (undefined === process.env.NODE_ENV) {
+  exec('husky install', (error, stdout) => {
+    if (error) {
+      // When the package is installed as a dependency, it also tries to install husky which fails. We ignore the error.
+      if (error.message.indexOf(".git can't be found") > -1) {
+        console.log('Skipping husky in the dependency.');
+        return process.exit(0);
+      }
+      console.error(`postinstall exec error: ${error}`);
+      return process.exit(1);
+    }
+    console.log(stdout);
+    return process.exit(0);
+  });
+}
